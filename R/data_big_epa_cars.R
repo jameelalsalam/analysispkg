aca8783539d365171e42ed7ee82b455322cd48c6
Jameel Alsalam <alsalam.jameel@epa.gov>
2020-06-08 08:50:47 -0400

    placeholder dataset doc

---
+++ R/data_big_epa_cars.R
@@ -0,0 +1,7 @@
+
+#' Vehicle fuel economy data
+#'
+#' From fueleconomy.gov via Tidy Tuesday project
+#'
+#' @source \url{https://github.com/rfordatascience/tidytuesday/tree/master/data/2019/2019-10-15}
+"big_epa_cars"
